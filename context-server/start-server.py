2dd34ab450c91fc8db09cf2c73db54e916a4e189
icaroGS <icaro@bemobile.tech>
2024-08-24 00:19:33 -0300

    chore: Update MQTT client version in start-server.py

--- context-server/start-server.py
+++ context-server/start-server.py
@@ -392,7 +392,10 @@ def on_message(client, userdata, msg):
     #     print(e)
 
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+# client = mqtt.Client()
+
 client.on_connect = on_connect
 client.on_message = on_message
 
